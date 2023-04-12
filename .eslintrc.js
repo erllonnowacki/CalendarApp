bd11774974323fc5e590c895bf09753e006c0848
Erllon <erllonemanuel11@gmail.com>
2023-04-12 20:19:21 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+// module 1
+export const config = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+};
